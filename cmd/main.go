63bf9d576fb9fa2fa96617e1f06ac907ee4acba1
paulyan <paulyan@google.com>
2020-05-19 22:53:44 -0700

    add log

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"log"
+
 	"github.com/xifanyan/goav"
 )
 
 func main() {
+
 	cfg := goav.DbConfig{
 		Dialect:       "sqlite3",
 		ConnectionStr: "./testdata/test.db",
@@ -13,19 +16,13 @@ func main() {
 	db, _ := goav.NewDatabase(cfg)
 	db.Init()
 
-	req1 := goav.NewStockRequest(goav.TimeSeriesDailyAdjusted, "GDX", goav.NA, goav.FULL, goav.CSV, "61LA8W7JK9QYJEJN")
-	ts1 := goav.NewStockTimeSeries(req1, db)
-	ts1.Save()
+	symbols := []string{"GDX", "GLD", "SILJ", "SLV"}
 
-	req2 := goav.NewStockRequest(goav.TimeSeriesDailyAdjusted, "GLD", goav.NA, goav.FULL, goav.CSV, "61LA8W7JK9QYJEJN")
-	ts2 := goav.NewStockTimeSeries(req2, db)
-	ts2.Save()
+	for _, symbol := range symbols {
+		log.Printf("Processing %s ...", symbol)
+		req := goav.NewStockRequest(goav.TimeSeriesDailyAdjusted, symbol, goav.NA, goav.FULL, goav.CSV, "61LA8W7JK9QYJEJN")
+		ts := goav.NewStockTimeSeries(req, db)
+		ts.Save()
+	}
 
-	req3 := goav.NewStockRequest(goav.TimeSeriesDailyAdjusted, "SILJ", goav.NA, goav.FULL, goav.CSV, "61LA8W7JK9QYJEJN")
-	ts3 := goav.NewStockTimeSeries(req3, db)
-	ts3.Save()
-
-	req4 := goav.NewStockRequest(goav.TimeSeriesDailyAdjusted, "SLV", goav.NA, goav.FULL, goav.CSV, "61LA8W7JK9QYJEJN")
-	ts4 := goav.NewStockTimeSeries(req4, db)
-	ts4.Save()
 }
